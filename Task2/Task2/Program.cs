7325821d229e92d47097999d7b876e1778b4f4d5
MihailMochalov <mihailmocelov@gmail.com>
2022-04-08 22:45:18 +0500

    Задание 2

---
+++ Task2/Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Programm
+    {
+        public static void Main()
+        {
+            double Rebro;
+
+            Console.WriteLine("ВВедите ребро октаэдр");
+            Rebro = Convert.ToDouble(Console.ReadLine());
+            double S;
+            double V;
+            double a = 3;
+            double c = 2;
+            S = (Math.Pow(Rebro, c) * Math.Sqrt(a)) * 2;
+            V = (Math.Pow(Rebro, a) * Math.Sqrt(c)) / 3;
+            Console.WriteLine($"Ответ: Площадь равна {S} Объем равен {V}");
+        }
+    }
+}
