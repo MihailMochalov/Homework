05c359b045b765966da555575b00f3952862cd6e
MihailMochalov <mihailmocelov@gmail.com>
2022-04-08 18:24:55 +0500

    Задание 1

---
+++ Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Мцыри М.Ю. лермонтов");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Однажды русский генерал Из гор к Тифлису проезжал");
+            Console.ResetColor();
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
