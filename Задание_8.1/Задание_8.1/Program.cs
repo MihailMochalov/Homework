005774b163c220d0c38474495fe2fe59a7389bc7
MihailMochalov <mihailmocelov@gmail.com>
2022-04-13 13:56:42 +0500

    Задание_8.1

---
+++ Задание_8.1/Задание_8.1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_8._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите переменную: ");
+            int a, b, n;
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите переменную: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            int sum;
+            sum = 0;
+            while (a <= b)
+            {
+                n = (int)Math.Pow(a, 3);
+                a++;
+                sum = sum + n;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
